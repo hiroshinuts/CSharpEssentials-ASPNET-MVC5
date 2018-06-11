33aaabafaf83cf3156a3aa337039cfa5bbd014fd
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2018-06-11 13:51:07 -0300

    BLL - ADO.NET

---
+++ Business Object - Acesso via ADO NET/Mvc_BO/BLL/AlunoBLL.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class AlunoBLL
+    {
+        public IEnumerable<Aluno> getAlunos()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["ConSQLServer"].ConnectionString;
+
+            List<Aluno> alunos = new List<Aluno>();
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("GetAlunos", con);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        Aluno aluno = new Aluno();
+                        aluno.Id = Convert.ToInt32(rdr["Id"]);
+                        aluno.Nome = rdr["Nome"].ToString();
+                        aluno.Email = rdr["Email"].ToString();
+                        aluno.Idade = Convert.ToInt32(rdr["Idade"]);
+                        aluno.DataInscricao = Convert.ToDateTime(rdr["DataInscricao"]);
+                        aluno.Sexo = rdr["Sexo"].ToString();
+                        alunos.Add(aluno);
+                    }
+                }
+                return alunos;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
