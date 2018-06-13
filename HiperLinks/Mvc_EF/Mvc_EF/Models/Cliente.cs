57f096f1bcf53c854c5c935f04a5c75e5cd0dc5c
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2018-06-13 18:53:25 -0300

    Criando HiperLinks

---
+++ HiperLinks/Mvc_EF/Mvc_EF/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_EF.Models
+{
+    [Table("Clientes")]
+    public class Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+    }
+}
