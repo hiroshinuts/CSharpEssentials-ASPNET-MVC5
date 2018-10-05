c88f41486fa86ff24fac3ef0bf132cee820a08df
Rafael Hiroshi <hiroshi_nuts@hotmail.com>
2018-10-05 16:21:40 -0300

    Web Api com seguranca OAuth

---
+++ FuncionariosAPIService/FuncionariosAPIService/FuncionariosAPIService/Startup.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(FuncionariosAPIService.Startup))]
+namespace FuncionariosAPIService
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // configuracao WebApi
+        var config = new HttpConfiguration();
+ 
+        // configurando rotas
+        config.MapHttpAttributeRoutes();
+        config.Routes.MapHttpRoute(
+            name: "DefaultApi",
+            routeTemplate: "api/{controller}/{id}",
+            defaults: new { id = RouteParameter.Optional }
+        );
+        // ativando cors
+        app.UseCors(CorsOptions.AllowAll);
+
+        //ativando a geração dos tokens de acesso
+        AtivarGeracaoTokenAcesso(app);
+
+        // ativando configuração WebApi
+        app.UseWebApi(config);
+        }
+
+        private void AtivarGeracaoTokenAcesso(IAppBuilder app)
+        {
+            //cria uma instância das opções de autorização
+            var opcoesConfiguracaoToken = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                // uri/token
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(1),
+                Provider = new ProviderDeTokensDeAcesso()
+            };
+
+            //realiza o processamento do request
+            // da autorização e do token
+            app.UseOAuthAuthorizationServer(opcoesConfiguracaoToken);
+            // compreende formato do token no header da requisicao
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+        }
+    }
+}
---
+++ Razor e comandos/Mvc_Razor/Mvc_Razor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc_Razor.Startup))]
+namespace Mvc_Razor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
